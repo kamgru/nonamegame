6f14ba50be6a42270d3a50bd38b253a84a84ab36
kamgru <kamil.grudzien@hotmail.com>
2017-05-23 18:30:50 +0200

    add tile type flags

--- Game1/Data/Tile.cs
+++ Game1/Data/Tile.cs
@@ -1,9 +1,20 @@
-﻿namespace Game1.Data
+﻿using System;
+
+namespace Game1.Data
 {
     public class Tile
     {
         public int X { get; set; }
         public int Y { get; set; }
         public int Value { get; set; }
+        public TileType TileType { get; set; }
+    }
+
+    [Flags]
+    public enum TileType
+    {
+        Normal = 1,
+        Start = 2,
+        End = 4
     }
 }
