d95dfcd57cf397c0225e2d7f72dbb27959f033f3
kamgru <kamil.grudzien@hotmail.com>
2017-05-29 19:39:56 +0200

    rename project

--- Game1.Core/Properties/AssemblyInfo.cs
+++ Game1.Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Game1.Common")]
+[assembly: AssemblyTitle("Game1.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Game1.Common")]
+[assembly: AssemblyProduct("Game1.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
