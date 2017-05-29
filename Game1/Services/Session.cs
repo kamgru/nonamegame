d0363ea99b466c3a03d714fd467b5765e7fb7a25
Kamil Grudzień <kamil.grudzien@hotmail.com>
2017-05-29 16:03:25 +0200

    implement basic session object

---
+++ Game1/Services/Session.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1.Services
+{
+    public class Session
+    {
+        private static Dictionary<string, Entry> _entries = new Dictionary<string, Entry>();
+
+        void Set<T>(string key, T value)
+        {
+            _entries.Add(key, new Entry
+            {
+                Type = typeof(T),
+                Value = value
+            });
+        }
+
+        T Get<T>(string key)
+        {
+            var entry = _entries[key];
+            if (entry.Type == typeof(T))
+            {
+                return (T) entry.Value;
+            }
+
+            throw new InvalidCastException(typeof(T).ToString());
+        }
+
+        public bool Contains<T>(string key)
+        {
+            return _entries.ContainsKey(key) && _entries[key].Type == typeof(T);
+        }
+
+        private class Entry
+        {
+            public Type Type { get; set; }
+            public object Value { get; set; }
+        }
+    }
+}
