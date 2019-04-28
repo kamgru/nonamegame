fcb03d3e1fef4f0cf282d56c7870a3399f4c4ef8
Kamil Grudzień <kamil.grudzien@hotmail.com>
2019-04-28 11:34:20 +0200

    fix build

--- NoNameGame.Main/Screens/MainMenuScreen.cs
+++ NoNameGame.Main/Screens/MainMenuScreen.cs
@@ -20,64 +20,46 @@ namespace NoNameGame.Main.Screens
 
         public override void Init()
         {
-            //_menuFont = ContentManager.Load<SpriteFont>("default");
-            //InputMapProvider.GetContextById(Contexts.Menu)?.Activate();
+            _menuFont = ContentManager.Load<SpriteFont>("default");
+            InputMapProvider.GetContextById(Contexts.Menu)?.Activate();
 
-            //_menu = new Menu(IntentProvider, SpriteBatch, _menuFont);
+            _menu = new Menu(IntentProvider, SpriteBatch, _menuFont);
 
-            //var viewport = ScreenManager.Game.GraphicsDevice.Viewport;
-            //_menu.Position = new Vector2(viewport.Width / 2, viewport.Height / 2);
+            var viewport = ScreenManager.Game.GraphicsDevice.Viewport;
+            _menu.Position = new Vector2(viewport.Width / 2, viewport.Height / 2);
 
-            //var start = new MenuItem { Text = "Start" };
+            var start = new MenuItem { Text = "Start" };
 
-            //start.OnSelected += (sender, args) =>
-            //{
-            //    InputMapProvider.GetContextById(Contexts.Menu).Deactivate();
-            //    ScreenManager.Push<GameplayScreen>();
-            //};
+            start.OnSelected += (sender, args) =>
+            {
+                InputMapProvider.GetContextById(Contexts.Menu).Deactivate();
+                ScreenManager.Push<GameplayScreen>();
+            };
 
-            //var quit = new MenuItem { Text = "Quit" };
-            //quit.OnSelected += (sender, args) =>
-            //{
-            //    ScreenManager.Game.Exit();
-            //};
+            var quit = new MenuItem { Text = "Quit" };
+            quit.OnSelected += (sender, args) =>
+            {
+                ScreenManager.Game.Exit();
+            };
 
-            //_menu.AddMenuItem(start);
-            //_menu.AddMenuItem(quit);
+            _menu.AddMenuItem(start);
+            _menu.AddMenuItem(quit);
 
             base.Init();
         }
-
-        private string labelText = "dupa";
 
         public override void Update(GameTime gameTime, bool isActive)
         {
             if (isActive)
             {
-                //_menu.Update(gameTime);
-
-                
-
-                if (ECS.Ui.Gui.Button(new Rectangle(10, 10, 64, 32), "left"))
-                {
-                    labelText = "chuj";
-                }
-
-                if (ECS.Ui.Gui.Button(new Rectangle(310, 10, 64, 32), "right"))
-                {
-                    labelText = "cipa";
-                }
-
-                Gui.Label(new Vector2(350, 100), labelText, Color.Red);
-
-                Gui.Slider(new Rectangle(20, 90, 20, 256), 100, 50);
+                _menu.Update(gameTime);
             }
         }
 
         public override void Draw(GameTime gameTime)
         {
-            //SpriteBatch.DrawString(_menuFont, "THE GAME", Vector2.Zero, Color.Black);
-            //_menu.Draw(gameTime);
+            SpriteBatch.DrawString(_menuFont, "THE GAME", Vector2.Zero, Color.Black);
+            _menu.Draw(gameTime);
         }
     }
 }
